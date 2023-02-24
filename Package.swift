8788acdd021afb2e50302e929061714e2c874179
iggy890 <98705626+iggy890@users.noreply.github.com>
2023-02-24 21:01:18 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,7 +12,17 @@ let package = Package(
         .target(name: "TreeSitterSQL",
                 path: ".",
                 exclude: [
+                    "bindings",
+                    "Cargo.toml",
+                    "corpus",
+                    "examples",
+                    "grammar.js",
+                    "LICENSE",
+                    "Makefile",
+                    "package.json",
                     "README.md",
+                    "src/grammar.json",
+                    "src/node-types.json",
                     // any additional files to exclude 
                 ],
                 publicHeadersPath: "bindings/swift",

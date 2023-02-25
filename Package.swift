2f37c5f2e513e2d401f096167dba6092014ce29b
iggy890 <98705626+iggy890@users.noreply.github.com>
2023-02-25 11:40:53 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,15 +1,14 @@
-// swift-tools-version:5.3
 import PackageDescription
 
-let package = Package(
-    name: "TreeSitterSQL",
-    platforms: [.macOS(.v10_13), .iOS(.v11)],
-    products: [
-        .library(name: "TreeSitterSQL", targets: ["TreeSitterSQL"]),
-    ],
-    dependencies: [],
-    targets: [
-        .target(name: "TreeSitterSQL",
+ let package = Package(
+     name: "TreeSitterSQL",
+     platforms: [.macOS(.v10_13), .iOS(.v11)],
+     products: [
+         .library(name: "TreeSitterSQL", targets: ["TreeSitterSQL"]),
+     ],
+     dependencies: [],
+     targets: [
+         .target(name: "TreeSitterSQL",
                 path: ".",
                 publicHeadersPath: "bindings/swift",
                 cSettings: [.headerSearchPath("src")])

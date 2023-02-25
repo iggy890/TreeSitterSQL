a9438a3d36ba715948a3c68bb96cb35172cd341f
iggy890 <98705626+iggy890@users.noreply.github.com>
2023-02-25 11:49:37 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,15 +1,38 @@
+// swift-tools-version:5.3
 import PackageDescription
 
- let package = Package(
-     name: "TreeSitterSQL",
-     platforms: [.macOS(.v10_13), .iOS(.v11)],
-     products: [
-         .library(name: "TreeSitterSQL", targets: ["TreeSitterSQL"]),
-     ],
-     dependencies: [],
-     targets: [
-         .target(name: "TreeSitterSQL",
+let package = Package(
+    name: "TreeSitterSQL",
+    platforms: [.macOS(.v10_13), .iOS(.v11)],
+    products: [
+        .library(name: "TreeSitterSQL", targets: ["TreeSitterSQL"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "TreeSitterSQL",
                 path: ".",
+                exclude: [
+                    "binding.gyp",
+                    "bindings",
+                    "Cargo.toml",
+                    "corpus",
+                    "examples",
+                    "grammar.js",
+                    "LICENSE",
+                    "Makefile",
+                    "package.json",
+                    "README.md",
+                    "src/grammar.json",
+                    "src/node-types.json",
+                    // any additional files to exclude 
+                ],
+                sources: [
+                    "src/parser.c",
+                    "src/scanner.cc", // this might be `scanner.c` or not present at all
+                ],
+                resources: [
+                    .copy("queries")
+                ],
                 publicHeadersPath: "bindings/swift",
                 cSettings: [.headerSearchPath("src")])
     ]
